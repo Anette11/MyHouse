3e8e79c3f50330fe9cc373914784af04d43d37ce
Anette11 <krivaltseva@gmail.com>
2023-04-11 23:30:16 +0400

    Create composable SmallCard

---
+++ app/src/main/java/com/example/myhouse/ui/screens/common/SmallCard.kt
@@ -0,0 +1,68 @@
+package com.example.myhouse.ui.screens.common
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myhouse.R
+
+@Composable
+fun SmallCard(
+    text: String,
+    @DrawableRes icon: Int
+) = Card(
+    modifier = Modifier
+        .fillMaxWidth()
+        .background(colorResource(id = R.color.white)),
+    shape = RoundedCornerShape(dimensionResource(id = R.dimen._16dp)),
+    elevation = dimensionResource(id = R.dimen._4dp)
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                vertical = dimensionResource(id = R.dimen._21dp),
+                horizontal = dimensionResource(id = R.dimen._16dp)
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text,
+            fontFamily = FontFamily(Font(R.font.circle_regular)),
+            fontSize = dimensionResource(id = R.dimen._17sp).value.sp,
+            color = colorResource(id = R.color.gray_darker)
+        )
+        Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen._16dp)))
+        IconButton(onClick = { }) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = stringResource(id = R.string.empty),
+                tint = colorResource(id = R.color.blue_sky)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SmallCardPreview() =
+    SmallCard(
+        text = stringResource(id = R.string.text_example),
+        icon = R.drawable.ic_lock
+    )
