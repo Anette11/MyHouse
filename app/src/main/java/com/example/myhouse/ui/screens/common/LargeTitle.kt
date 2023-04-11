7b0e77bc78bbb04900e08d76cbcb85cf8378a262
Anette11 <krivaltseva@gmail.com>
2023-04-11 20:51:06 +0400

    Create folder common. Create LargeTitle composable

---
+++ app/src/main/java/com/example/myhouse/ui/screens/common/LargeTitle.kt
@@ -0,0 +1,35 @@
+package com.example.myhouse.ui.screens.common
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myhouse.R
+
+@Composable
+fun LargeTitle(
+    text: String
+) = Text(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = dimensionResource(id = R.dimen._29dp)),
+    text = text,
+    fontSize = dimensionResource(id = R.dimen._21sp).value.sp,
+    fontFamily = FontFamily(Font(R.font.circle)),
+    color = colorResource(id = R.color.gray_dark),
+    textAlign = TextAlign.Center
+)
+
+@Composable
+@Preview(showBackground = true)
+fun LargeTitlePreview() =
+    LargeTitle(text = stringResource(id = R.string.my_house))
