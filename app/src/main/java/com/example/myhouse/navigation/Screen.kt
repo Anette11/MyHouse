06900a1fe8ac1a279adc0f74aa62199913c5f8f7
Anette11 <krivaltseva@gmail.com>
2023-04-12 21:40:44 +0400

    Delete HouseNavHost as not needed

--- app/src/main/java/com/example/myhouse/navigation/Screen.kt
+++ app/src/main/java/com/example/myhouse/navigation/Screen.kt
@@ -1,16 +1,13 @@
 package com.example.myhouse.navigation
 
 sealed class Screen(
-    val route: String,
     val tabName: String
 ) {
     object Cameras : Screen(
-        route = "cameras_route",
         tabName = "Камеры"
     )
 
     object Doors : Screen(
-        route = "doors_route",
         tabName = "Двери"
     )
 }
--- app/src/main/java/com/example/myhouse/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/myhouse/ui/screens/main/MainViewModel.kt
@@ -4,33 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.myhouse.di.DefaultDispatcher
 import com.example.myhouse.navigation.Screen
-import com.example.myhouse.util.launch
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import javax.inject.Inject
 
-@HiltViewModel
-class MainViewModel @Inject constructor(
-    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
-) : ViewModel() {
+class MainViewModel : ViewModel() {
 
     var selectedTabIndex by mutableStateOf(0)
         private set
 
     fun onTabSelected(index: Int) {
         selectedTabIndex = index
-        launch(defaultDispatcher) {
-            _navigateToRoute.emit(screens[index].route)
-        }
     }
-
-    private val _navigateToRoute: MutableSharedFlow<String> = MutableSharedFlow()
-    val navigateToRoute: SharedFlow<String> = _navigateToRoute.asSharedFlow()
 
     val screens = listOf(
         Screen.Cameras,
