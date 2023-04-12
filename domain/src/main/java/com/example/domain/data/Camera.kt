917ff016382558be17c13782bf9c81862dbead7e
Anette11 <krivaltseva@gmail.com>
2023-04-12 15:01:16 +0400

    Create data classes for cameras and doors in domain layer

---
+++ domain/src/main/java/com/example/domain/data/Camera.kt
@@ -0,0 +1,10 @@
+package com.example.domain.data
+
+data class Camera(
+    val favorites: Boolean?,
+    val id: Int?,
+    val name: String?,
+    val rec: Boolean?,
+    val room: String?,
+    val snapshot: String?
+)
---
+++ domain/src/main/java/com/example/domain/data/Door.kt
@@ -0,0 +1,9 @@
+package com.example.domain.data
+
+data class Door(
+    val favorites: Boolean?,
+    val id: Int?,
+    val name: String?,
+    val room: String?,
+    val snapshot: String?
+)
