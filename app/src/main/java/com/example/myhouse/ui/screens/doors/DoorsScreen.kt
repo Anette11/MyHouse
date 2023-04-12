b7aa9c5a1cc3943dff9297ea7ac5e4333f9de261
Anette11 <krivaltseva@gmail.com>
2023-04-12 20:48:21 +0400

    Add horizontal padding for composable DoorsScreen

--- app/src/main/java/com/example/myhouse/ui/screens/doors/DoorsScreen.kt
+++ app/src/main/java/com/example/myhouse/ui/screens/doors/DoorsScreen.kt
@@ -1,11 +1,14 @@
 package com.example.myhouse.ui.screens.doors
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myhouse.R
 import com.example.myhouse.ui.screens.common.LargeCardSimple
 import com.example.myhouse.ui.screens.common.SmallCard
 import com.example.myhouse.ui.screens.util.ScreenItem
@@ -14,7 +17,9 @@ import com.example.myhouse.ui.screens.util.ScreenItem
 fun DoorsScreen(
     viewModel: DoorsViewModel = hiltViewModel()
 ) = LazyColumn(
-    modifier = Modifier.fillMaxWidth()
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = dimensionResource(id = R.dimen._21dp))
 ) {
     items(viewModel.screenItems) { screenItem ->
         when (screenItem) {
