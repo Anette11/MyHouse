209ceb4850a826ea4cb6576a4cc83be90bd223e8
Anette11 <krivaltseva@gmail.com>
2023-04-18 00:11:04 +0400

    Delete annotations for IODispatcher and SingleThreadExecutor as not used

--- data/src/main/java/com/example/data/di/CoroutineDispatchersAnnotations.kt
+++ data/src/main/java/com/example/data/di/CoroutineDispatchersAnnotations.kt
@@ -5,14 +5,4 @@ import javax.inject.Qualifier
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.RUNTIME)
-annotation class IODispatcher
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
 annotation class MainDispatcher
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-annotation class SingleThreadExecutor
--- data/src/main/java/com/example/data/di/CoroutineDispatchersModule.kt
+++ data/src/main/java/com/example/data/di/CoroutineDispatchersModule.kt
@@ -6,25 +6,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.Executors
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class CoroutineDispatchersModule {
 
     @Provides
-    @IODispatcher
-    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
-
-    @Provides
     @MainDispatcher
     fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
-
-    @Provides
-    @Singleton
-    @SingleThreadExecutor
-    fun provideSingleThreadExecutor(): CoroutineDispatcher =
-        Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 }
